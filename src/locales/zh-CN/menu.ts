b5dd9623f3b2b690ed2153ae1107b619b47afaa5
cd yang <1012892053@qq.com>
2021-03-15 17:51:42 +0800

    add string resources

--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -49,4 +49,6 @@ export default {
   'menu.editor.flow': '流程编辑器',
   'menu.editor.mind': '脑图编辑器',
   'menu.editor.koni': '拓扑编辑器',
+  'menu.basic-diagram': 'basic-diagram',
+  'menu.extend-diagram': 'extend-diagram',
 };
