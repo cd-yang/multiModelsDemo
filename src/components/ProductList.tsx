2da5477aa3038091bdc698186e34b4df3fa0b0c2
cd yang <1012892053@qq.com>
2021-03-15 17:53:45 +0800

    add test component

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { Table, Popconfirm, Button } from 'antd';
+import type { ListItem } from '@/models/basicDiagram';
+
+export type ProductListProps = {
+  onDelete: (id: number) => void;
+  products: ListItem[];
+};
+
+function ProductList({ onDelete, products }: ProductListProps) {
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }, {
+    title: 'Actions',
+    render: (text: string, record: ListItem) => {
+      return (
+        <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+          <Button>Delete</Button>
+        </Popconfirm>
+      );
+    },
+  }];
+
+  return (
+    <Table
+      dataSource={products}
+      columns={columns}
+    />
+  );
+}
+
+ProductList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  products: PropTypes.array.isRequired,
+};
+
+export default ProductList;
